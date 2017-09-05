6ba19be452d8bad7df4b23a54aafe77ec4627d4c
Stella Harrison <sh16g14@soton.ac.uk>
2017-09-05 17:01:13 +0100

    Update TMP36.ino

--- TMP36.ino
+++ TMP36.ino
@@ -4,15 +4,11 @@ void setup() {
   delay(10000); // 10 second delay before data transfer starts
 }
 void loop() {
-  float reading = analogRead(A0);
-  float voltage = (reading/1024.0)*5.0; //voltage
-  float temp = (voltage-0.55)*100.0; //convert to temp
-  String volt_t1=String(voltage);
-  String temp1=String(temp);
-  Serial.println(temp1);
-  delay(250); //quarter second delay between each data point 
-  Serial.println(volt_t1)
-  delay(250);
-  
+  int reading = analogRead(A0); 
+  float voltage = reading * 3.3;
+  voltage /= 1024.0;
+  float temperatureC = (voltage - 0.5) * 100 ;
+  Serial.println(temperatureC);
+  delay(1000);  
 }
 
