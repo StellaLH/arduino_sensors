2422a312792dbc99afc3296a127768e21d31d4b3
Stella Harrison <sh16g14@soton.ac.uk>
2017-09-07 09:54:32 +0100

    Update sensor_plot.py

--- sensor_plot.py
+++ sensor_plot.py
@@ -18,8 +18,8 @@ t1=0
 t1v=0
 t2=0
 t2v=0
-h=0
-hv=0
+h1=0
+v1=0
 grapher=0
 
 data_t1=np.array(list(csv.reader(open("t1_sensor_data.txt", "rb"), delimiter=","))).astype("float") # open .csv file from folder
@@ -28,67 +28,77 @@ data_h1=np.array(list(csv.reader(open("h1_sensor_data.txt", "rb"), delimiter=","
 data_v1=np.array(list(csv.reader(open("v1_sensor_data.txt", "rb"), delimiter=","))).astype("float")
 
 
-time_xdata=[]
+time_xdata_t1=[]
+time_xdata_t2=[]
+time_xdata_h1=[]
+time_xdata_v1=[]
+
 xdata_t1=data_t1[:,0]
 xdata_t2=data_t1[:,0]
 xdata_h1=data_h1[:,0]
+xdata_v1=data_v1[:,0]
 
 
 while grapher !='pyplot' and grapher != 'plotly':
     grapher=raw_input("Please choose a grapher: plotly or pyplot?\n")
-points=input("How many data points would you like to see?(Max. = %s)\n" %data.shape[0])
+
+points=input("-------------------------------------------------\nNumber of data points acquired from each sensor:\ntemperature 1 = %s\ntemperature 2 = %s\nhumidity 1 = %s\nvibration 1 = %s\n\nHow many data points would you like to see?\n" %(len(data_t1), len(data_t2), len(data_h1), len(data_v1)))
+
 while t1!='y'and t1!='n':
-    t1=raw_input("Would you like to view temperature 1? [y/n]\n")
-while t1v!='y'and t1v!='n':
-    t1v=raw_input("Would you like to view temperature 1 voltages? [y/n]\n")
+    t1=raw_input("-------------------------------------------------\nWould you like to view temperature 1? [y/n]\n")
+
 while t2!='y'and t2!='n':
-    t2=raw_input("Would you like to view temperature 2? [y/n]\n")
-while t2v!='y'and t2v!='n':
-    t2v=raw_input("Would you like to view temperature 2 voltages? [y/n]\n")
-while h!='y'and h!='n':
-    h=raw_input("Would you like to view relative humidity? [y/n]\n") 
-while hv!='y'and hv!='n':
-    hv=raw_input("Would you like to view relative humidity voltages? [y/n]\n") 
+    t2=raw_input("-------------------------------------------------\nWould you like to view temperature 2? [y/n]\n")
+
+while h1!='y'and h1!='n':
+    h1=raw_input("-------------------------------------------------\nWould you like to view humidity 1? [y/n]\n")
+        
+while v1!='y'and v1!='n':
+    v1=raw_input("-------------------------------------------------\nWould you like to view vibration 1? [y/n]\n") 
+print "-------------------------------------------------\nYour plot is being processed..."
+
 for q in xdata_t1:
     time_xdata_t1.append(datetime.strptime("%s"%str(int(q)), "%Y%m%d%H%M%S"))
+
 for r in xdata_t2:
-    time_xdata_t2.append(datetime.strptime("%s"%str(int(q)), "%Y%m%d%H%M%S"))
+    time_xdata_t2.append(datetime.strptime("%s"%str(int(r)), "%Y%m%d%H%M%S"))
+
 for s in xdata_h1:
-    time_xdata_h1.append(datetime.strptime("%s"%str(int(q)), "%Y%m%d%H%M%S"))
+    time_xdata_h1.append(datetime.strptime("%s"%str(int(s)), "%Y%m%d%H%M%S"))
+    
+for t in xdata_v1:
+    time_xdata_v1.append(datetime.strptime("%s"%str(int(t)), "%Y%m%d%H%M%S"))
 
 
 if grapher == 'pyplot':
     pytrace1=data_t1[points*-1:,1]
-    pytrace2=data_t1[points*-1:,2]
+
     pytrace3=data_t2[points*-1:,1]
-    pytrace4=data_t2[points*-1:,2]
+
     pytrace5=data_h1[points*-1:,1]
-    pytrace6=data_h1[points*-1:,2]
+    
+    pytrace7=data_v1[points*-1:,1]
+
     
     if t1=='y':
         plt.plot(time_xdata_t1[points*-1:],pytrace1, label="Temp 1")
     else:
         pass
-    if t1v=='y':
-        plt.plot(time_xdata_t1[points*-1:],pytrace2, label ="Temp 1 Voltages")
-    else:
-        pass
+
     if t2=='y':
         plt.plot(time_xdata_t2[points*-1:],pytrace3, label="Temp 2")
     else:
         pass
-    if t2v=='y':
-        plt.plot(time_xdata_t2[points*-1:],pytrace4, label="Temp 2 Voltages")
-    else:
-        pass
-    if h=='y':
+
+    if h1=='y':
         plt.plot(time_xdata_h1[points*-1:],pytrace5, label="Humidity")
     else:
         pass
-    if hv=='y':
-        plt.plot(time_xdata_h1[points*-1:],pytrace6, label= "Humidity Voltages")
+    if v1=='y':
+        plt.plot(time_xdata_v1[points*-1:],pytrace7, label="Vibration")
     else:
-        pass   
+        pass
+ 
     plt.legend(bbox_to_anchor=(0.5, 1),loc=8, ncol=2
     )
     plt.show()
@@ -105,16 +115,7 @@ elif grapher=='plotly':
         sensortraces.append(trace1)
     else:
         pass
-    if t1v=='y':
-        trace2=Scatter(
-        x=time_xdata_t1[points*-1:],
-        y= data_t1[points*-1:,2],
-        mode='lines+markers',
-        name='Temperature 1 Voltage'
-    )
-        sensortraces.append(trace2)
-    else:
-        pass
+
     if t2=='y':
         trace3=Scatter(
         x=time_xdata_t2[points*-1:],
@@ -126,17 +127,8 @@ elif grapher=='plotly':
         sensortraces.append(trace3)
     else:
         pass
-    if t2v=='y':
-        trace4=Scatter(
-        x=time_xdata_t2[points*-1:],
-        y= data_t2[points*-1:,2],
-        mode='lines+markers',
-        name='Temperature 2 Voltage'
-    )
-        sensortraces.append(trace4)
-    else:
-        pass
-    if h=='y':
+
+    if h1=='y':
         trace5=Scatter(
         x=time_xdata_h1[points*-1:],
         y= data_h1[points*-1:,1],
@@ -146,16 +138,18 @@ elif grapher=='plotly':
         sensortraces.append(trace5)
     else:
         pass
-    if hv=='y':
-        trace6=Scatter(
-        x=time_xdata_h1[points*-1:],
-        y= data_h1[points*-1:,2],
+    
+    if v1=='y':
+        trace7=Scatter(
+        x=time_xdata_v1[points*-1:],
+        y= data_v1[points*-1:,1],
         mode='lines+markers',
-        name='Humidity Voltage\n (%)'
+        name='vibration'
     )
-        sensortraces.append(trace6)
+        sensortraces.append(trace7)
     else:
-        pass   
+        pass
+
   
     
     title='Sensor Temperature'
