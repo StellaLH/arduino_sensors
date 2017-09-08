e11afc14d0055d19fb4645789839720ef852a0e8
Stella Harrison <sh16g14@soton.ac.uk>
2017-09-08 14:08:38 +0100

    Update temp_hum_volts_plotly.py

--- temp_hum_volts_plotly.py
+++ temp_hum_volts_plotly.py
@@ -16,14 +16,14 @@ import serial
 import time as t
 import csv
 import numpy as np
-from plotly.plotly import plot, iplot
+from plotly.offline import plot, iplot
 from plotly.graph_objs import Scatter, Figure, Layout
 from datetime import datetime
 
 #import plotly.plotly as py
 
 #setup 
-serial_port='/dev/ttyACM0' # check on Arduino IDE which port used
+serial_port='/dev/ttyUSB0' # check on Arduino IDE which port used
 baud_rate=9600
 k=[]
 l=[]
@@ -40,47 +40,47 @@ try:
     while True:
         with open('sensor_data.txt','a') as f:
 
-            ser=serial.Serial(serial_port, baud_rate)
-            #k=append(i)
-
-            
-            line=ser.readline()
-            line=str(line.decode("utf-8"))
-            l=(float(line))
-
-            
-            line=ser.readline()
-            line=float(line.decode("utf-8"))
-            m=(float(line))
-            
-            line=ser.readline()
-            line=float(line.decode("utf-8"))
-            n=(float(line))
-            
-            line=ser.readline()
-            line=float(line.decode("utf-8"))
-            o=(float(line))
-            
-            line=ser.readline()
-            line=float(line.decode("utf-8"))
-            p=(float(line))
-            
-            line=ser.readline()
-            line=float(line.decode("utf-8"))
-            q=(float(line))
-            
-            
-            
-            time_str=t.strftime("%Y%m%d%H%M%S")
-            f.write("%s," %time_str) #time stamp
-            f.write("%s," %l) #temp1
-            f.write("%s," %m) #temp1 votlage
-            f.write("%s," %n) #temp2
-            f.write("%s," %o) #temp2 voltage
-            f.write("%s," %p) #humidity
-            f.write("%s" %q) #humidity voltage
-            f.write("\n")
-            f.close()
+#            ser=serial.Serial(serial_port, baud_rate)
+#            #k=append(i)
+#
+#            
+#            line=ser.readline()
+#            line=str(line.decode("utf-8"))
+#            l=(float(line))
+#
+#            
+#            line=ser.readline()
+#            line=float(line.decode("utf-8"))
+#            m=(float(line))
+#            
+#            line=ser.readline()
+#            line=float(line.decode("utf-8"))
+#            n=(float(line))
+#            
+#            line=ser.readline()
+#            line=float(line.decode("utf-8"))
+#            o=(float(line))
+#            
+#            line=ser.readline()
+#            line=float(line.decode("utf-8"))
+#            p=(float(line))
+#            
+#            line=ser.readline()
+#            line=float(line.decode("utf-8"))
+#            q=(float(line))
+#            
+#            
+#            
+#            time_str=t.strftime("%Y%m%d%H%M%S")
+#            f.write("%s," %time_str) #time stamp
+#            f.write("%s," %l) #temp1
+#            f.write("%s," %m) #temp1 votlage
+#            f.write("%s," %n) #temp2
+#            f.write("%s," %o) #temp2 voltage
+#            f.write("%s," %p) #humidity
+#            f.write("%s" %q) #humidity voltage
+#            f.write("\n")
+#            f.close()
             print "data saved"
             
         data=np.array(list(csv.reader(open("sensor_data.txt", "rb"), delimiter=","))).astype("float") # open .csv file from folder 
@@ -133,10 +133,10 @@ try:
         t.sleep(898.4) #make up to 15 minutes per acquisition
 except KeyboardInterrupt:   
     print "\nFinishing acquisition..."
-    data=np.array(list(csv.reader(open("sensor_data.txt", "rb"), delimiter=","))).astype("float") # open .csv file from folder 
-    time_xdata=[]
-    xdata=data[-48:,0]
-    for q in xdata:
+data=np.array(list(csv.reader(open("sensor_data.txt", "rb"), delimiter=","))).astype("float") # open .csv file from folder 
+time_xdata=[]
+xdata=data[-48:,0]
+for q in xdata:
         time_xdata.append(datetime.strptime("%s"%str(int(q)), "%Y%m%d%H%M%S"))
         
     trace1=Scatter(
@@ -147,7 +147,7 @@ except KeyboardInterrupt:
     )
     trace2=Scatter(
         x=time_xdata,
-        y= data[-49:,2],
+        y= data[-48:,2],
         mode='lines+markers',
         name='Temperature 1 Voltage'
     )
@@ -180,4 +180,4 @@ except KeyboardInterrupt:
     voltagetraces=[trace2,trace4,trace6] #for voltages plot
     title='Sensor Temperature'
     plot(sensortraces, auto_open=False, filename='sensor_data.html')
-         
+     
